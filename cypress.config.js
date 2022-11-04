62362a5f8d0464abb036d2163a51d1273946b737
neolao <contact@neolao.com>
2022-11-04 15:30:17 +0100

    up

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   video: true,
-  videoCompression: false,
+  videoCompression: 16,
   userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36',
   e2e: {
     supportFile: false,
--- cypress/e2e/extract.cy.js
+++ cypress/e2e/extract.cy.js
@@ -5,28 +5,21 @@ describe('Extract last post', () => {
   it('passes', () => {
     const loginUrl = 'https://www.linkedin.com/login/fr?fromSignIn=true&trk=guest_homepage-basic_nav-header-signin';
     const companyUrl = 'https://www.linkedin.com/company/swile/posts/?feedView=all';
-    cy.visit(loginUrl)
+    const visitOptions = {
+      failOnStatusCode: false,
+      timeout: 60000
+    };
+    cy.visit(loginUrl, visitOptions)
 
     cy.get('#username').type(config.username)
     cy.get('#password').type(config.password)
     cy.get('button[type="submit"]').click()
 
-    cy.visit(
-        companyUrl, 
-        {
-            failOnStatusCode: false,
-            timeout: 60000
-        }
-    );
+    cy.wait(10000);
 
-    //cy.get('.main-feed-activity-card').invoke('text').then((text) => {
-    
-    /*
-    cy.get('.feed-shared-update-v2 .feed-shared-update-v2__description-wrapper').invoke('text').then((text) => {
-        console.log('hop', text);
-    });
-    */
-    cy.get('.feed-shared-update-v2 .feed-shared-update-v2__description-wrapper').then(function($elem) {
+    cy.visit(companyUrl, visitOptions);
+
+    cy.get('.feed-shared-update-v2 .feed-shared-update-v2__description-wrapper', { timeout: 10000 }).then(function($elem) {
         const hop = $elem.text();
         cy.log(hop)
         cy.task('log', hop)
