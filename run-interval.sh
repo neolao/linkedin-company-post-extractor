202562572e57050fc97fefcd95b0e7a3547436c9
neolao <contact@neolao.com>
2022-11-07 08:58:11 +0100

    up

--- run-interval.sh
+++ run-interval.sh
@@ -8,6 +8,7 @@ do
 
   nextDate=$(date +"%Y-%m-%d %T" -d "+6 hours")
   echo "Next run: $nextDate"
-  # 60 * 60 * 6 = 21600
-  sleep 21600
+  # 60 * 60 * 6  = 21600
+  # 60 * 60 * 24 = 86400
+  sleep 86400
 done
